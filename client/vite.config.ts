536d9afd899732d5d2ad3466951f042f43cb0706
thinkless-azone <thinkless@gmail.com>
2026-02-18 15:28:09 +0300

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,8 @@
+typescript
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/aiagent-proposal/',
-  build: {
-    outDir: '../dist/public'
-  }
+  base: '/aiagent-proposal/'
 })
